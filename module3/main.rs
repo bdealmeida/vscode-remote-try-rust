7cbf44465681dd3c495dbac9b3baf1f2362e3255
bdealmeida <b.dealmeida1999@gmail.com>
2024-09-30 18:56:22 +0000

    Completed I/O example

--- module3/main.rs
+++ module3/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::{Write, BufReader, BufRead};
+
 fn add3(n: u32) -> (u32, u32) {
     return (n, n+3);
 }
@@ -10,10 +13,33 @@ struct Person {
 
 fn main() {
     let x = 5;
-    println!("{} -> {:?}", x, add3(x));
+    println!("{} -> {:?}\n\n", x, add3(x));
 
     let name = String::from("Tara");
     let age = 65;
-    let tara = Person{name, age};
-    println!("Name: {}\nAge: {}", tara.name, tara.age);
+    //let tara = Person{name, age};
+    //println!("Name: {}\nAge: {}", tara.name, tara.age);
+
+    let mut write_fd = File::create("person-info").unwrap();
+    writeln!(write_fd, "{}\n{}", name, age).unwrap();
+
+    // Open file to read
+    let read_fd = File::open("person-info").unwrap();
+    let mut reader = BufReader::new(read_fd);
+    let mut buf = String::new();
+
+    // Read in name
+    let mut _len = reader.read_line(&mut buf).unwrap();
+    buf.pop(); // read_line includes \n in buf
+    let read_name = buf.clone(); // Save name from buf
+    buf.clear(); // reset buf to empty string
+
+    // Read in age
+    _len = reader.read_line(&mut buf).unwrap();
+    buf.pop();
+    let read_age = buf.parse::<u8>().unwrap();
+
+    // Create struct with read in values
+    let tara = Person{name: read_name, age: read_age};
+    println!("{} {}", tara.name, tara.age - 60);
 }
